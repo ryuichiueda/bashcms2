8652b1290af0564f0a26784e26b0e874a6e5e3db
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-08-14 09:18:35 +0900

    Add scripts

---
+++ scripts/counter.bash
@@ -0,0 +1,8 @@
+#!/bin/bash -xv
+exec 2> /tmp/log
+source /var/www/bashcms2/conf
+
+while read url ; do
+	page=$(tr -dc 'a-zA-Z0-9_?=/' <<< "$url" | sed 's/.*?//' | sed 's;=;s/;' )
+	ls -l "$datadir/$page/views" | cut -d' ' -f 5
+done
