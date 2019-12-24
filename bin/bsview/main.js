b48ebc9553c6d463b48e8adbd3cfd12c7336d7f6
Ryuichi Ueda <ryuichiueda@gmail.com>
2019-12-24 11:43:16 +0900

    Purge Google Analytics

--- bin/bsview/main.js
+++ bin/bsview/main.js
@@ -1,10 +1,6 @@
 window.onload = function () {
-    if(document.cookie.indexOf("cookieconfirm=") < 0){
-        window['ga-disable-UA-39565206-5'] = true;
+    if(document.cookie.indexOf("cookieconfirm=") < 0)
         document.getElementById("cookiemention").style.visibility = "visible";
-    }else if(document.cookie.indexOf("cookieconfirm=ng") > 0){
-        window['ga-disable-UA-39565206-5'] = true;
-    }
 
     var toc = document.getElementById("toc");
     if(toc.getElementsByTagName("li").length < 2)
@@ -17,7 +13,6 @@ window.onload = function () {
     lastRead(10); 
     rankArticles(10); 
     linkKeywords();
-//    fullSearch("");
 }                     
                       
 function lastArticles(num){
@@ -126,21 +121,6 @@ document.getElementById("cookieok").onclick = function() {
     var expire = new Date();
     expire.setMonth(expire.getMonth() + 3); 
     document.cookie = "cookieconfirm=ok; expires=" + expire.toUTCString();
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)};
-    gtag('js', new Date());
-  
-    gtag('config', 'UA-39565206-5');
 }
-
-document.getElementById("cookieng").onclick = function() {
-    document.getElementById("cookiemention").hidden = true;
-    var expire = new Date();
-    expire.setMonth(expire.getMonth() + 3); 
-    document.cookie = "cookieconfirm=ng; expires=" + expire.toUTCString();
-}
-
 
 setInterval(lastRead, 3000, 10);
-
